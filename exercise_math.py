3b24d801ccdc3de261666a1b437c34485b3c80fa
maxicoleur <maximocoleur@gmail.com>
2026-03-12 19:46:41 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -13,3 +13,19 @@ def math():
     """
     a = 57
     b = 7
+
+    la_suma=a+b
+    la_diferencia=a-b
+    el_producto=a*b
+    el_promedio= (a+b)/2
+    el_cociente_entero=a//b
+    el_resto_de_la_division_entera=a%b
+    el_valor_real_de_la_division_entera=a/b
+
+    print(la_suma)
+    print(la_diferencia)
+    print(el_producto)
+    print(el_promedio)
+    print(el_cociente_entero)
+    print(el_resto_de_la_division_entera)
+    print(el_valor_real_de_la_division_entera)
