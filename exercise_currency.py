59e98121fdf4182856f17e03e72b17bb72c667f5
maxicoleur <maximocoleur@gmail.com>
2026-03-12 19:45:20 -0300

    Update exercise_currency.py

--- exercise_currency.py
+++ exercise_currency.py
@@ -11,3 +11,11 @@ def currency():
     tasa_dolar = 1500  # 1 dólar = 1500 pesos
     tasa_euro = 1600   # 1 euro = 1600 pesos
     tasa_real = 250    # 1 real = 250 pesos
+
+    monto_dolares=10000/1500
+    monto_euros=10000/1600
+    monto_real=10000/250
+
+    print(monto_dolares)
+    print(monto_euros)
+    print(monto_real)
